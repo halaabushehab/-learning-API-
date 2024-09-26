f5f34dddf9e8417490defc6362287b63555c2b60
halaabushehab <hala.abushiihab@gmail.com>
2024-09-26 18:13:06 +0300

    divide

---
+++ main.js
@@ -0,0 +1,19 @@
+$('#calc_btn').on('click',()=>{
+ let first_input=$('#first_input ').val();
+ let second_input=$('#second_input ').val();
+
+ let result = divide((first_input), (second_input))
+ if(result != 'unknown' ){
+    $('#Result ').html(result);
+
+ }
+
+})
+
+function divide(N1,N2){
+    if(N2==0){
+       alert("you can't divide by zero") 
+    return "unknown"
+    }
+return N1/N2
+}
